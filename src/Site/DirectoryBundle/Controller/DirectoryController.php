103e331a84995c45b65b3dff230ef650e2485a12
Paul RENVOISE <prenvois@e2r10p1.42.fr>
2014-05-18 18:13:30 +0200

    [Fix] comment form in forum, regexp in requirements for showOther route

--- src/Site/DirectoryBundle/Controller/DirectoryController.php
+++ src/Site/DirectoryBundle/Controller/DirectoryController.php
@@ -6,26 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DirectoryController extends Controller
 {
-    public function indexAction()
-    {
-        $username = $this->container->getParameter('ldap_login');
-        $password = $this->container->getParameter('ldap_password');
+	public function indexAction()
+	{
+		$username = $this->container->getParameter('ldap_login');
+		$password = $this->container->getParameter('ldap_password');
 
-        $ldaphost = "ldap.42.fr";
+		$ldaphost = "ldap.42.fr";
 		$ldapconn = ldap_connect($ldaphost);
 
-        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-        $ldapbind = ldap_bind($ldapconn, $username, $password);
+		ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+		$ldapbind = ldap_bind($ldapconn, $username, $password);
 
-        $dn = "dc=42,dc=fr";
+		$dn = "dc=42,dc=fr";
 		$filter="(&(objectClass=ft-user)(!(close=*))(ou:dn:=2013)(picture=*)(homeDirectory=*))";
-        $sr = ldap_search($ldapconn, $dn, $filter);
+		$sr = ldap_search($ldapconn, $dn, $filter);
 
-        $info = ldap_get_entries($ldapconn, $sr);
+		$info = ldap_get_entries($ldapconn, $sr);
 
-        //print_r($info);
-
-        foreach ($info as $i => $user)
+		foreach ($info as $i => $user)
 		{
 			if (isset($user['picture']))
 			{
@@ -33,6 +31,6 @@ class DirectoryController extends Controller
 			}
 		}
 
-        return $this->render('SiteDirectoryBundle:Directory:index.html.twig', array('users' => $info));
-    }
+		return $this->render('SiteDirectoryBundle:Directory:index.html.twig', array('users' => $info));
+	}
 }
