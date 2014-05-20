484867f66ddec6c706630506cc75b2776bba7728
Paul RENVOISE <prenvois@e2r9p6.42.fr>
2014-05-20 14:56:34 +0200

    [Add] PlanningBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -41,6 +41,7 @@ class AppKernel extends Kernel
             new Site\DirectoryBundle\SiteDirectoryBundle(),
             new Site\ActivityBundle\SiteActivityBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new Site\PlanningBundle\SitePlanningBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
