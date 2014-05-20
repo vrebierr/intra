a0baf56e884f7d390768e6ba6e6d88de8e213875
Valentin REBIERRE <vrebierr@e2r9p2.42.fr>
2014-05-20 21:21:20 +0200

    [Add] module & activity registration

--- src/Site/ActivityBundle/Controller/ActivityController.php
+++ src/Site/ActivityBundle/Controller/ActivityController.php
@@ -3,6 +3,9 @@
 namespace Site\ActivityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Site\ActivityBundle\Entity\Module;
+use Site\ActivityBundle\Entity\Activity;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ActivityController extends Controller
 {
@@ -19,7 +22,7 @@ class ActivityController extends Controller
 		return $this->render('SiteActivityBundle:Activities:index.html.twig', $data);
 	}
 
-	public function moduleAction(\Site\ActivityBundle\Entity\Module $module)
+	public function moduleAction(Module $module)
 	{
 		$data = array();
 
@@ -32,7 +35,27 @@ class ActivityController extends Controller
 		return $this->render('SiteActivityBundle:Activities:module.html.twig', $data);
 	}
 
-	public function activityAction(\Site\ActivityBundle\Entity\Activity $activity)
+	public function moduleRegistrationAction(Module $module)
+	{
+		$now = new \Datetime("NOW");
+		$user = $this->container->get("security.context")->getToken()->getUser();
+		// if is too early or late to registratration
+		if ($now > $module->getEndRegistration() || $now < $module->getStartRegistration())
+			throw new AccessDeniedException("You can't register for this activity now.");
+
+		if ($module->getStudents()->contains($user))
+			$module->removeStudent($user);
+		else
+			$module->addStudent($user);
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($module);
+		$em->flush();
+
+		return $this->redirect($this->generateUrl('site_activities_module', array('id' => $module->getId())));
+	}
+
+	public function activityAction(Activity $activity)
 	{
 		$data = array();
 
@@ -43,5 +66,28 @@ class ActivityController extends Controller
 		$data['module'] = $activity->getModule();
 
 		return $this->render('SiteActivityBundle:Activities:activity.html.twig', $data);
+	}
+
+	public function activityRegistrationAction(Activity $activity)
+	{
+		$now = new \Datetime("NOW");
+		$user = $this->container->get("security.context")->getToken()->getUser();
+		// if is too early or late to registratration
+		if ($now > $activity->getEndRegistration() || $now < $activity->getStartRegistration())
+			throw new AccessDeniedException("You can't register for this activity now.");
+		
+		if (!$activity->getModule()->getStudents()->contains($user))
+			throw new AccessDeniedException("You can't register for this activity before register module");
+
+		if ($activity->getStudents()->contains($user))
+			$activity->removeStudent($user);
+		else
+			$activity->addStudent($user);
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($activity);
+		$em->flush();
+
+		return $this->redirect($this->generateUrl('site_activities_activity', array('id' => $activity->getId())));
 	}
 }
