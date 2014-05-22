50f4f574c92b56022705fa5900e246b574fede89
Paul RENVOISE <prenvois@e1r4p2.42.fr>
2014-05-22 19:35:18 +0200

    [NAdd] zeroclipboard to copy autologinurl

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 	});
 	$('.ui.sortable').tablesort();
 	$(".refresh.icon").popup();
+	$(".copy.icon").popup();
 	$(".part").popup();
 	$(".donught.chart").knob({
 		readOnly:true,
