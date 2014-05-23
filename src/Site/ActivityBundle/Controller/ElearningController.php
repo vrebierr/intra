02aca80f56db847927a24b46aff378c9b711fac4
Yann PRINGAULT <ypringau@e2r9p3.42.fr>
2014-05-23 18:01:58 +0200

    [Update] Elearning WIP

--- src/Site/ActivityBundle/Controller/ElearningController.php
+++ src/Site/ActivityBundle/Controller/ElearningController.php
@@ -20,6 +20,32 @@ class ElearningController extends Controller
         $data['activity'] = null;
         $data['module'] = null;
 
-        return $this->render('SiteActivityBundle:Elearning:elearning.html.twig', $data);
+        return $this->render('SiteActivityBundle:Elearning:index.html.twig', $data);
+    }
+
+    public function activityAction(Activity $activity)
+    {
+        $data = array();
+
+        $modules = $this->getDoctrine()->getManager()->getRepository("SiteActivityBundle:Module")->findAll();
+
+        $data['modules'] = $modules;
+        $data['activity'] = $activity;
+        $data['module'] = null;
+
+        return $this->render('SiteActivityBundle:Elearning:activity.html.twig', $data);
+    }
+
+    public function moduleAction(Module $module)
+    {
+        $data = array();
+
+        $modules = $this->getDoctrine()->getManager()->getRepository("SiteActivityBundle:Module")->findAll();
+
+        $data['modules'] = $modules;
+        $data['activity'] = null;
+        $data['module'] = $module;
+
+        return $this->render('SiteActivityBundle:Elearning:module.html.twig', $data);
     }
 }
