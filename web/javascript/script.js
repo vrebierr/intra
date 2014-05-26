dee90af006da716fd0d4b18a74021051c4a4f803
Melodie SAGODIRA <msagodir@e2r9p1.42.fr>
2014-05-26 17:04:07 +0200

    planning

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -30,4 +30,20 @@ $(document).ready(function() {
 	{
 		$(location).attr('href', "./?range="+$(this).attr("data-text"));
 	});
+
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    var h = {};
+
+    $("#calendar").fullCalendar({
+        header: h,
+        selectable: true,
+        editable: true,
+        draggable: true,
+        droppable: false
+    });
+
+
 });
