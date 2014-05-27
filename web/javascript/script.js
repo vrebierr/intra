7b2f3dcbc67ac891973341ecf12240b9d08af3cf
Paul RENVOISE <prenvois@e2r10p1.42.fr>
2014-05-27 12:42:32 +0200

    [Fix] add month/week/day view in calendar

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
+
+	/* DROPDOWNS */
 	$('.ui.dropdown').dropdown();
 	$('.ui.selection.dropdown').dropdown();
+
+	/* ACCORDION */
 	$('.ui.accordion').accordion();
+
+	/* TRANSITIONS */
 	$('.ui.blue.button.show.form').click(function() {
 		$('.form.thread').transition('slide down');
 	});
 	$('.ui.blue.tiny.button.show.comment').click(function() {
 		$(this).parents('.post').children('.form.comment').transition('slide down');
 	});
-	$('.ui.sortable').tablesort();
-	$(".refresh.icon").popup();
-	$(".part").popup();
-	$(".donught.chart").knob({
-		readOnly:true,
-		fgColor:"#6ECFF5"
-	});
-
 	$('.show.activities').click(function() {
 		if ($(this).attr("class") == "show activities")
 			$(this).attr("class", "show activities active");
@@ -27,20 +25,39 @@ $(document).ready(function() {
 			$(this).parents('tbody').children("."+$(this).attr("data-text")).attr("class", "hidden "+$(this).attr("data-text"));
 	});
 
-	$('.directory_option').click(function ()
-	{
+	/* SORT */
+	$('.ui.sortable').tablesort();
+
+	/* RANGE */
+	$('.directory_option').click(function(){
 		$(location).attr('href', "./?range="+$(this).attr("data-text"));
 	});
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-    var h = {};
-    $("#calendar").fullCalendar({
-        header: h,
-        selectable: true,
-        editable: true,
-        draggable: true,
-        droppable: false
-    });
+
+	/* POPUPS */
+	$(".refresh.icon").popup();
+	$(".part").popup();
+
+	/* KNOB */
+	$(".donught.chart").knob({
+		readOnly:true,
+		fgColor:"#6ECFF5"
+	});
+
+	/* CALENDAR */
+	var date = new Date();
+	var d = date.getDate();
+	var m = date.getMonth();
+	var y = date.getFullYear();
+	var h = {
+		left: 'title',
+		center: '',
+		right: 'month,agendaWeek,agendaDay, today, prev,next'
+	};
+	$("#calendar").fullCalendar({
+		header: h,
+		selectable: true,
+		editable: true,
+		draggable: true,
+		droppable: false
+	});
 });
