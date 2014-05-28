a96ce713b6d6698ba1ab6f7e97065f03bd9d6bc1
Yann PRINGAULT <ypringau@e2r9p3.42.fr>
2014-05-28 15:51:59 +0200

    [Add] Group view

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -105,6 +105,7 @@ $(document).ready(function() {
         if ($(this).val() != "")
 		{
 			$('.error-group').hide();
+			$('.validate-group').removeAttr("style");
 			$('.validate-group').prop("disabled", false);
 		}
     });
