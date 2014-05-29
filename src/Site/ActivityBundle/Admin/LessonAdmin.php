755c0a7ed6f3ccb9a47f59a659a386bc8d2fb27c
Paul RENVOISE <prenvois@e2r10p1.42.fr>
2014-05-29 16:14:27 +0200

    [Add] translation for elearning in Admin Panel

--- src/Site/ActivityBundle/Admin/LessonAdmin.php
+++ src/Site/ActivityBundle/Admin/LessonAdmin.php
@@ -10,75 +10,80 @@ use Sonata\AdminBundle\Show\ShowMapper;
 
 class LessonAdmin extends Admin
 {
-    /**
-     * @param DatagridMapper $datagridMapper
-     */
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper
-            ->add('activity')
-            ->add('name')
-            ->add('description')
-            ->add('link')
-            ->add('type')
-        ;
-    }
+	protected $translationDomain = 'SiteElearningBundle';
 
-    /**
-     * @param ListMapper $listMapper
-     */
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper
-            ->add('activity')
-            ->add('name')
-            ->add('description')
-            ->add('link')
-            ->add('type')
-            ->add('_action', 'actions', array(
-                'actions' => array(
-                    'show' => array(),
-                    'edit' => array(),
-                    'delete' => array(),
-                )
-            ))
-        ;
-    }
+	/**
+	 * @param DatagridMapper $datagridMapper
+	 */
+	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+	{
+		$datagridMapper
+			->add('activity', null, array('label' => 'ADMIN_ELEARNING_ACTIVITY'))
+			->add('name', null, array('label' => 'ADMIN_ELEARNING_NAME'))
+			->add('description', null, array('label' => 'ADMIN_ELEARNING_DESCRIPTION'))
+			->add('link', null, array('label' => 'ADMIN_ELEARNING_LINK'))
+			->add('type', null, array('label' => 'ADMIN_ELEARNING_TYPE'))
+		;
+	}
 
-    /**
-     * @param FormMapper $formMapper
-     */
-    protected function configureFormFields(FormMapper $formMapper)
-    {
-        $formMapper
-            ->add('activity')
-            ->add('name')
-            ->add('description', 'textarea', array(
-                'attr' => array(
-                    'class' => 'tinymce'
-                )))
-            ->add('link', 'sonata_media_type', array(
-                'provider' => 'sonata.media.provider.file',
-                'context'  => 'default',
-                'required' => false
-            ))
-            ->add('type', 'choice', array(
-                'choices' => array('pdf' => 'PDF', 'video' => 'Vidéo', 'html' => 'Page HTML')
-                ))
-        ;
-    }
+	/**
+	 * @param ListMapper $listMapper
+	 */
+	protected function configureListFields(ListMapper $listMapper)
+	{
+		$listMapper
+			->add('activity', null, array('label' => 'ADMIN_ELEARNING_ACTIVITY'))
+			->add('name', null, array('label' => 'ADMIN_ELEARNING_NAME'))
+			->add('description', null, array('label' => 'ADMIN_ELEARNING_DESCRIPTION'))
+			->add('link', null, array('label' => 'ADMIN_ELEARNING_LINK'))
+			->add('type', null, array('label' => 'ADMIN_ELEARNING_TYPE'))
+			->add('_action', 'actions', array(
+				'actions' => array(
+					'show' => array(),
+					'edit' => array(),
+					'delete' => array(),
+				)
+			))
+		;
+	}
 
-    /**
-     * @param ShowMapper $showMapper
-     */
-    protected function configureShowFields(ShowMapper $showMapper)
-    {
-        $showMapper
-        ->add('activity')
-        ->add('name')
-        ->add('description')
-        ->add('link')
-        ->add('type')
-        ;
-    }
+	/**
+	 * @param FormMapper $formMapper
+	 */
+	protected function configureFormFields(FormMapper $formMapper)
+	{
+		$formMapper
+			->add('activity', null, array('label' => 'ADMIN_ELEARNING_ACTIVITY'))
+			->add('name', null, array('label' => 'ADMIN_ELEARNING_NAME'))
+			->add('description', 'textarea', array(
+				'label' => 'ADMIN_ELEARNING_DESCRIPTION',
+				'attr' => array(
+					'class' => 'tinymce'
+				)))
+			->add('link', 'sonata_media_type', array(
+				'label' => 'ADMIN_ELEARNING_LINK',
+				'provider' => 'sonata.media.provider.file',
+				'context'  => 'default',
+				'required' => false
+			))
+			->add('type', 'choice', array(
+				'label' => 'ADMIN_ELEARNING_TYPE',
+				'choices' => array('pdf' => 'PDF', 'video' => 'Vidéo', 'html' => 'Page HTML')
+				))
+		;
+	}
+
+	/**
+	 * @param ShowMapper $showMapper
+	 */
+	protected function configureShowFields(ShowMapper $showMapper)
+	{
+		$showMapper
+		->add('activity', null, array('label' => 'ADMIN_ELEARNING_ACTIVITY'))
+		->add('name', null, array('label' => 'ADMIN_ELEARNING_NAME'))
+		->add('description', null, array('label' => 'ADMIN_ELEARNING_DESCRIPTION'))
+		->add('link', null, array('label' => 'ADMIN_ELEARNING_LINK'))
+		->add('type', null, array('label' => 'ADMIN_ELEARNING_TYPE'))
+		;
+	}
 }
