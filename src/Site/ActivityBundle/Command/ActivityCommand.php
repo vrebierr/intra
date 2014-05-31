5406605ecba47f118397300ca995a490fbc0bc2a
Valentin REBIERRE <vrebierr@student.42.fr>
2014-05-31 18:02:37 +0200

    [Add] command generatePeers

--- src/Site/ActivityBundle/Command/ActivityCommand.php
+++ src/Site/ActivityBundle/Command/ActivityCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Site\ActivityBundle\Entity\ScaleGroup;
 
 class ActivityCommand extends ContainerAwareCommand
 {
@@ -26,7 +27,37 @@ class ActivityCommand extends ContainerAwareCommand
 		foreach($activities as $activity)
 		{
 			if ($activity->getEnd() < $date && $activity->getEndCorrection() > $date)
+			{
+				$em = $this->getContainer()->get('doctrine')->getManager();
+				$groups = $em->getRepository('SiteActivityBundle:ActivityGroup')->findBy(array("activity" => $activity));
+				$scale = $em->getRepository('SiteActivityBundle:Scale')->findOneBy(array("activity" => $activity));
+				$students = $activity->getStudents()->toArray();
+				shuffle($groups);
+				shuffle($students);
+				foreach ($students as $student)
+				{
+					for ($i = 0; $i < 4; $i++)
+					{
+						$scale_group = new ScaleGroup();
+						$rand = rand(0, count($groups) - 1);
+						while ($groups[$rand]->getStudents()->contains($student))
+						{
+							if (!isset($groups[1]))
+								break;
+							$rand = rand(0, count($groups) - 1);
+						}
+						$scale_group->setGroup($groups[$rand]);
+						$scale_group->setRater($student);
+						$scale_group->setScale($scale);
+						$em->persist($scale_group);
+						$groups[$rand]->peers += 1;
+						if ($groups[$rand]->peers == 4)
+							$groups = array_splice($groups, $rand);
+					}
+				}
+				$em->flush();
 				$output->writeln($activity->getName());
+			}
 		}
     }
 }
--- src/Site/ActivityBundle/Controller/ScaleController.php
+++ src/Site/ActivityBundle/Controller/ScaleController.php
@@ -68,37 +68,4 @@ class ScaleController extends Controller
 			"group" => $group
 			));
 	}
-
-	public function generatePeersAction(Activity $activity)
-	{
-		$em = $this->getDoctrine()->getManager();
-		$groups = $em->getRepository('SiteActivityBundle:ActivityGroup')->findBy(array("activity" => $activity));
-		$scale = $em->getRepository('SiteActivityBundle:Scale')->findOneBy(array("activity" => $activity));
-		$students = $activity->getStudents()->toArray();
-		shuffle($groups);
-		shuffle($students);
-		foreach ($students as $student)
-		{
-			for ($i = 0; $i < 4; $i++)
-			{
-				$scale_group = new ScaleGroup();
-				$rand = rand(0, count($groups) - 1);
-				while ($groups[$rand]->getStudents()->contains($student))
-				{
-					if (!isset($groups[1]))
-						break;
-					$rand = rand(0, count($groups) - 1);
-				}
-				$scale_group->setGroup($groups[$rand]);
-				$scale_group->setRater($student);
-				$scale_group->setScale($scale);
-				$em->persist($scale_group);
-				$groups[$rand]->peers += 1;
-				if ($groups[$rand]->peers == 4)
-					$groups = array_splice($groups, $rand);
-			}
-		}
-		$em->flush();
-		return;
-	}
 }
